dec89ffeb6e6a6b6ea3da341e423f1c3e4195076
Lisa Le <lisale841@gmail.com>
2021-12-06 13:02:03 -0800

    Completed javascript-operators-and-expression exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,38 @@
+var width = 10;
+var height = 2;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 5;
+var payment = 10;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 2;
+var midterm = 5;
+var final = 8;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Lisa';
+var lastName = 'Le';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var ph = 5;
+var isAcidic = ph < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 8;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
