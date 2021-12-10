ed6b45316760888af1341450a85ddc0a6d04ec62
Lisa Le <lisale841@gmail.com>
2021-12-09 17:36:45 -0800

    WIP

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,9 +1,8 @@
 /* exported getIndexes */
-
 function getIndexes(array) {
   var indexList = [];
   for (var i = 0; i < array.length; i++) {
-    indexList.push(array.indexof(i));
+    indexList.push(array.indexof(array[i]));
   }
   return indexList;
 }
