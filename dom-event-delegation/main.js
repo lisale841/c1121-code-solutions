875faa947a722805a9aefd65c538ebbce9b0b60d
Lisa Le <lisale841@gmail.com>
2021-12-16 19:08:13 -0800

    Completed dom-event-delegation exercise.

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,18 +1,14 @@
 var $taskList = document.querySelector('.task-list');
-// var $ul = document.querySelector('ul');
 
 function clicker(event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
 
   if (event.target.name !== 'BUTTON') {
-    //   console.log((targetElement.closest('li')
-    // };
-
+    var $taskList = event.target.closest('.task-list-item');
+    console.log($taskList);
+    $taskList.remove();
   }
 }
 
 $taskList.addEventListener('click', clicker);
-// Get the event.target's closest('.task-list-item') ancestor element.
-// Log that.task - list - item element to the console.It should be the parent list item element of the clicked button.
-//     remove() that.task - list - item element from the DOM
