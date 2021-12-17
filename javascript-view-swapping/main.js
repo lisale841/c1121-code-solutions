027ae980440f493ac74e659e1d7ab1d590984d42
Lisa Le <lisale841@gmail.com>
2021-12-16 17:29:53 -0800

    Completed javascript-view-swapping.

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,31 @@
+// notes:
+// add a click to tab container and create a function.
+// check to see what we click is a tab
+// if it a tab then. loop thru the tabs to see which one is active
+// if active then make the class active if not then just tab.
+// only if tab was clicked
+//    - get the alue of data view attribute of the event target and put it in var
+//    - loop thru nodelist of view & compare each of their data view
+// do the same as previous but get the view if the current tab is clicked then
+// the information should pop up. information is put into view class.
+
+var $tabContainer = document.querySelector('.tab-container');
+var $tab = document.querySelectorAll('.tab');
+var $view = document.querySelectorAll('.view');
+
+function clicker(event) {
+  var activeTab = event.target.matches('.tab');
+  if (activeTab) {
+    for (var i = 0; i < $tab.length; i++) {
+      if ($tab[i] === event.target) {
+        $tab[i].className = 'tab active';
+        $view[i].className = 'view';
+      } else if ($tab[i] !== event.target) {
+        $tab[i].className = 'tab';
+        $view[i].className = 'view hidden';
+      }
+    }
+  }
+}
+
+$tabContainer.addEventListener('click', clicker);
