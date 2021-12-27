41dd79ba77072b6c76b69802b6cb692849e46733
Lisa Le <lisale841@gmail.com>
2021-12-27 10:01:00 -0800

    WIP

--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,1 +1,13 @@
+/* input: array, count
+/* -create a storage to store the new list
+   - */
 /* exported take */
+function take(array, count) {
+  var newArray = [];
+
+  for (var i = 0; i <= count - 1; i++) {
+    newArray.push(array[i]);
+
+  }
+  return newArray;
+}
