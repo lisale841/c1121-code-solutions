c01e18f19a6916ab08f424a80603fc9f8c0e3fe4
Lisa Le <lisale841@gmail.com>
2021-12-29 10:08:14 -0800

    WIP

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -6,15 +6,13 @@ function Bank() {
 }
 
 Bank.prototype.openAccount = function (holder, balance) {
-  for (var i = 0; i <= balance.length - 1; i++) {
-    // console.log(balance.length);
-    // console.log(balance);
-    if (balance[i] > 0 && balance[i] % 1 === 0) {
-      this.accounts.push(balance[i]);
-      return true;
-    }
+
+  if (balance > 0 && balance % 1 === 0) {
+    this.accounts.push(balance);
+    return true;
   }
   return null;
+
 };
 
 Bank.prototype.getAccount = function () {
