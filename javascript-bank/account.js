d6b96d107d467405ef146b49e327124aab6a910e
Lisa Le <lisale841@gmail.com>
2021-12-29 09:04:59 -0800

    WIP

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -8,16 +8,17 @@ function Account(number, holder) {
 
 Account.prototype.deposit = function (amount) {
 
-  if (amount > 0 && amount === true) {
-    return true;
+  if (amount > 0 && amount % 1 === 0) {
+    this.transactions.push(new Transaction('deposit', amount));
 
+    return true;
   }
   return false;
 };
 
 Account.prototype.withdraw = function (amount) {
-  if (amount > 0 && amount === true) {
-
+  if (amount > 0 && amount % 1 === 0) {
+    this.transactions.push(new Transaction('withdrawal', amount));
     return true;
   } else {
     return false;
@@ -27,5 +28,13 @@ Account.prototype.getBalance = function () {
   if (this.transactions.length === 0) {
     return 0;
   }
-
+  var balance = 0;
+  for (var i = 0; i <= this.transactions.length - 1; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      balance = balance + this.transactions[i].amount;
+    } else {
+      balance = balance - this.transactions[i].amount;
+    }
+  }
+  return balance;
 };
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -6,13 +6,17 @@ function Bank() {
 }
 
 Bank.prototype.openAccount = function (holder, balance) {
-  if (balance > 0 && balance === true) {
-    return true;
-
+  for (var i = 0; i <= balance.length - 1; i++) {
+    // console.log(balance.length);
+    // console.log(balance);
+    if (balance[i] > 0 && balance[i] % 1 === 0) {
+      this.accounts.push(balance[i]);
+      return true;
+    }
   }
   return null;
-
 };
+
 Bank.prototype.getAccount = function () {
   return null;
 };
