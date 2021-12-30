40178ae63316a8b0e551088e3bff228889cf739d
Lisa Le <lisale841@gmail.com>
2021-12-30 00:48:04 -0800

    Completed javascript-bank exercise.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -8,16 +8,41 @@ function Bank() {
 Bank.prototype.openAccount = function (holder, balance) {
 
   if (balance > 0 && balance % 1 === 0) {
-    this.accounts.push(balance);
-    return true;
+
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+
   }
   return null;
 
 };
 
-Bank.prototype.getAccount = function () {
+Bank.prototype.getAccount = function (number) {
+  // look thru the this.accounts one by one to get the number
+  // check if the account number matches the parameter number.
+  // return that account.
+  for (var i = 0; i <= this.accounts.length - 1; i++) {
+    if (this.accounts[i].number === number) {
+
+      return this.accounts[i];
+    }
+  }
   return null;
 };
+
 Bank.prototype.getTotalAssets = function () {
-  return 0;
+
+  var totalBalance = 0;
+  if (this.accounts.length === 0) {
+    return 0;
+  }
+  for (var i = 0; i <= this.accounts.length - 1; i++) {
+    totalBalance = totalBalance + this.accounts[i].getBalance();
+
+  }
+
+  return totalBalance;
 };
