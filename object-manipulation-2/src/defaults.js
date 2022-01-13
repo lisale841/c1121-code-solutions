2a2c02659acfb6fa3d7f2d186946e8812fc37a67
Lisa Le <lisale841@gmail.com>
2022-01-13 07:48:20 +0000

    Completed object-manipulation-2 exercise

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,1 +1,13 @@
+/* - look at the properties of source
+   - check to see if the property in target does not equal the property in source
+   - if it does not equal then we can add that property to target
+   */
 /* exported defaults */
+
+function defaults(target, source) {
+  for (var key in source) {
+    if ((key in target) !== (key in source)) {
+      target[key] = source[key];
+    }
+  }
+}
