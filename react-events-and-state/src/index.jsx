5331f7f1913aff49b105e4fa9190a6bc572a1272
Lisa Le <lisale841@gmail.com>
2022-02-03 01:13:40 +0000

    completed react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isClicked ? 'Thanks!' : 'Click Me!'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.getElementById('root')
+);
