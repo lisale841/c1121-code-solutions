a4c647c0d9a7373a4d2cac4e4acc55df2eafb0e9
Lisa Le <lisale841@gmail.com>
2022-02-08 19:29:19 +0000

    completed fetch-in-react-practice updated with data and code reading.

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
+// import React from react-packages
 import PageTitle from './page-title';
+// import pagetitle from page title module
 import TodoList from './todo-list';
 import TodoForm from './todo-form';
 
 export default class App extends React.Component {
+  // export default class App extends Component propety of React object.
+  // body
   constructor(props) {
+    // constructor method defined with one parameter props with code block.
     super(props);
+    // super function is being called with one argument props.
     this.state = {
+    // an object literal is being assigned to the state prop of the this object
       todos: []
-    };
+    // empty array assigned to todos property.
+    }; // closing object lit curly brack.
     this.addTodo = this.addTodo.bind(this);
+    // bind method of the addTodo prop of the this object is being called with the
+    // this argument and the return of the bind method is being assigned to the add todo prop of this object.
     this.toggleCompleted = this.toggleCompleted.bind(this);
+    // bind method of toggleCompleted prop of the this object is being called with one argument
+    // this and the return of the bind method is being assigned to the toggleCompleted prop of this obj.
   }
 
   componentDidMount() {
+    // the Component did Mount method is being defined with no parameters. code block.
     fetch('http://localhost:3000/api/todos')
+    // the fetch function is being called with one argument string url.
       .then(res => res.json())
+      // the then method of the return of the fetch call is being called with one argument
+      // anony function with one parameter res
+      // the json method of the res object is being called with no arg. and the return of the
+      // json method is being returned from the anony function.
       .then(data => this.setState({ todos: data }))
+      // the then method of the return of the prev. then method is being called with one argument
+      // anony function with one pareameter data.
+      // the setState method of the this object is being called with one argument object lit
+      // data value assigned to todos prop. and the return of the setState method is being
+      // returned from the function.
       .catch(error => console.error(error));
+    // the catch method of the return of the previous then method is being called with one argument
+    // anony function with one paremeter error
+    // the error method of the console object is being called with one arg error and the return of the
+    // error method is being returned from the function.
   }
 
   /**
@@ -59,9 +86,18 @@ export default class App extends React.Component {
     */
 
   toggleCompleted(todoId) {
+    // toggleCompleted Method is being defined with one parameter "todoId"
+    // body
     const item = this.state.todos.find(element => element.todoId === todoId);
+    // find method of the todos prop of the state prop of the this object is being
+    // called with one arg. anony function with one parameter element
+    // todoId prop of the element object is strictly equal to todo Id.
+    // the result of the expression is being returned from the function
+    // the return of the find method is being assigned to const variable item.
 
     fetch('http://localhost:3000/api/todos/' + todoId, {
+      // fetch function is being called with two argument string url concated with todoID
+      // object literal.
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
@@ -72,7 +108,7 @@ export default class App extends React.Component {
       .then(data => {
         const updatedList = this.state.todos.map(item => {
           if (item.todoId === data.todoId) {
-            item.isCompleted = data.isCompleted;
+            return data;
           }
           return item;
         });
