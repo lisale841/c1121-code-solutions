62a681c3bf1c091e6e4ec2d0cc61b7bd498ea97b
Lisa Le <lisale841@gmail.com>
2022-02-10 17:28:15 +0000

    api-authorization

--- api-authorization/server/authorization-middleware.js
+++ api-authorization/server/authorization-middleware.js
@@ -9,13 +9,9 @@ function authorizationMiddleware(req, res, next) {
     throw new ClientError(401, 'authentication required');
   }
 
-  try {
-    const decoded = jwt.verify(xToken, process.env.TOKEN_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    throw new ClientError(401, 'authentication required');
-  }
+  const decoded = jwt.verify(xToken, process.env.TOKEN_SECRET);
+  req.user = decoded;
+  next();
 
 }
 /* your code here */
